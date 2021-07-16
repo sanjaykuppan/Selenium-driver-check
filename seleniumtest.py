82de3c531cce684ed4262a25312027e34b7b21da
sk016 <sanjaykuppan94@gmail.com>
2021-07-16 18:55:03 +0530

    adding seleniumtest file

---
+++ seleniumtest.py
@@ -0,0 +1,68 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+import time
+import os.path
+import requests
+import zipfile
+import wget
+
+fname="chromedriver.exe"
+
+class Loaddriver:
+
+    def __init__(self):
+        self.fname="chromedriver.exe"
+
+    #get latest chrome driver 
+    def getdriver(self):
+        url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+        response = requests.get(url)
+        version_number = response.text
+        download_url = "https://chromedriver.storage.googleapis.com/" + version_number +"/chromedriver_win32.zip"
+        latest_driver_zip = wget.download(download_url,'chromedriver.zip')
+        #remove old version of chromedriver.exe
+        if os.path.isfile(self.fname):
+            os.remove(self.fname)
+        with zipfile.ZipFile(latest_driver_zip, 'r') as zip_ref:
+            zip_ref.extractall() # specify destination folder path here
+        os.remove(latest_driver_zip)
+        print("Driver updated successfully!")
+
+    #check driver version and browser version
+    def checkdriver(self):
+        def sampletest():
+            driver=webdriver.Chrome()
+            driver.maximize_window()
+            driver.get("https://www.google.com/")
+            driver.find_element_by_name("q").send_keys("github")
+            time.sleep(3)
+            driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
+            time.sleep(3)
+            driver.quit()
+            print("completed")
+
+        if os.path.isfile(self.fname):
+            try:
+                driver=webdriver.Chrome(self.fname)
+                driver.quit()
+            except Exception as e:
+                print(e.msg)
+                ind=e.msg.index("browser")
+                browserver=e.msg[ind+19:ind+31]
+                ind=e.msg.index("Chrome version")
+                drivever=e.msg[ind+15:ind+17]
+                print("Browser version :"+browserver)
+                print("Driver version :"+drivever)
+                self.getdriver()
+            finally:
+                sampletest()  
+
+        else:
+            self.getdriver()
+            sampletest()
+            
+
+
+if __name__=="__main__":
+    Loaddrver=Loaddriver()
+    Loaddrver.checkdriver()
