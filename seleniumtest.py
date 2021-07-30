9f9ec8ab82404db136b4b0b547825c50a5400cc7
sk016 <sanjaykuppan94@gmail.com>
2021-07-30 10:18:49 +0530

    commit

--- seleniumtest.py
+++ seleniumtest.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import SessionNotCreatedException
 import os.path
 import os
 import requests
